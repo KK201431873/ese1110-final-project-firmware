84640fc50b21a16486787eba73809c5cb96c69fd
Stephen Cai <KK201431873@gmail.com>
2025-11-13 02:05:50 -0500

    u

--- peripheral_firmware/peripheral_firmware.ino
+++ peripheral_firmware/peripheral_firmware.ino
@@ -180,6 +180,8 @@ void printIMU() {
 // =====================================================
 void processEncoder() {
   double now = millis()/1.0e3;
+  // leftEncoder += 20;
+  // rightEncoder += 20;
   leftEncoder += (int)(100 * (0.3*sin(0.103*now + 0.314*cos(0.063*now)) + 0.7*cos(0.1*tan(0.01*now) + 0.084*now + 0.143*cos(0.01*now))));
   rightEncoder += (int)(100 * (0.45*cos(0.03*now - 0.287*sin(0.021*now)) + 0.55*sin(0.153*now - 0.09*sin(0.061*now))));
 }
